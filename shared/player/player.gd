584e3d109bfffdbc110f5c487b4a9ba988d42a54
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2025-01-03 08:50:44 +0100

    feat: minor tweaks

--- shared/player/player.gd
+++ shared/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const FORWARD_SPEED = 7.0
 const BACKWARD_SPEED = 5.0
-const JUMP_VELOCITY = 8
+const JUMP_VELOCITY = 6
 var overlapping_cubes
 @onready var hitbox: Area3D = $Area3D
 var is_controllable = true
