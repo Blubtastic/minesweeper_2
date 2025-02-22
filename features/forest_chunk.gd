c20865152bbb9fcde7cb24bf6d1aa68b23c544e2
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2025-02-22 13:23:37 +0100

    fix: despawn forest chunks

--- features/forest_chunk.gd
+++ features/forest_chunk.gd
@@ -2,3 +2,7 @@ extends AnimatableBody3D
 
 func _physics_process(delta):
 	move_and_collide(Vector3(0, 0, Globals.world_speed*delta))
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
