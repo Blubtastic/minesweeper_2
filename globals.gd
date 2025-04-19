042c8fd74aee29c074872ca94805b40a97922179
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2025-04-19 16:51:55 +0200

    feat: global music code cleanup

--- globals.gd
+++ globals.gd
@@ -22,4 +22,3 @@ func set_music_low_pass_filter(enabled: bool):
 	var effect = AudioServer.get_bus_effect(bus_index, 0) # hard coded to first since we only have 1 atm.
 	if effect is AudioEffectLowPassFilter:
 		AudioServer.set_bus_effect_enabled(bus_index, 0, enabled)
-		effect.cutoff_hz = 1200 if enabled else 20500
