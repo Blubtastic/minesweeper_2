54905d0ec501c870435c0423171c7e06d3633b7e
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-01-23 09:37:05 +0100

    feat: app folder for core stuff

--- 2D/start_button.gd
+++ 2D/start_button.gd
@@ -1,4 +1,5 @@
 extends Button
+const MAIN = preload("uid://c1p7v7melj8n4")
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main.tscn")
+	get_tree().change_scene_to_packed(MAIN)
