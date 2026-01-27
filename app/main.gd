f7ecbff3cc77d902f5da8566e308abd94f7e86a8
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-01-27 19:42:38 +0100

    feat: stop using raycasts main

--- app/main.gd
+++ app/main.gd
@@ -4,27 +4,12 @@ const GAME_OVER = preload("uid://qhpvf7y1n474")
 const CUBE_CHUNK = preload("uid://dgxv52jn27v3c")
 const FOREST_CHUNK = preload("uid://dhlda46fqvnos")
 
-@onready var ray: RayCast3D = $Ray
-@onready var left_ray: RayCast3D = $LeftRay
-@onready var right_ray: RayCast3D = $RightRay
 @onready var cubechunk_timer: Timer = $CubechunkTimer
 
 func _ready():
 	Globals.game_ended.connect(_on_game_ended)
 	spawn_chunk_in(3)
 
-func _physics_process(_delta: float):
-	if !left_ray.is_colliding():
-		var forest_instance = FOREST_CHUNK.instantiate()
-		var forest_position = Vector3(ray.transform.origin.x - 9, 0.5, ray.transform.origin.z - 3.945)
-		forest_instance.transform.origin = forest_position
-		add_child(forest_instance)
-	if !right_ray.is_colliding():
-		var forest_instance = FOREST_CHUNK.instantiate()
-		var forest_position = Vector3(right_ray.transform.origin.x + 2, 0.5, right_ray.transform.origin.z - 3.945)
-		forest_instance.transform.origin = forest_position
-		add_child(forest_instance)
- 
 func _on_game_ended():
 	var game_over_instance = GAME_OVER.instantiate()
 	add_child(game_over_instance)
@@ -42,3 +27,15 @@ func spawn_cubechunk():
 	var chunk_position = Vector3(-4.5, 0, -20.85)
 	chunk_instance.transform.origin = chunk_position
 	add_child(chunk_instance)
+	spawn_forest()
+
+func spawn_forest():
+	var forest_instance = FOREST_CHUNK.instantiate()
+	var forest_position = Vector3(-9, 0.5,-17.42)
+	forest_instance.transform.origin = forest_position
+	add_child(forest_instance)
+	
+	var forest_instance2 = FOREST_CHUNK.instantiate()
+	var forest_position2 = Vector3(9, 0.5, -17.42)
+	forest_instance2.transform.origin = forest_position2
+	add_child(forest_instance2)
