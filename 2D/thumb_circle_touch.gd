1634f648c8057eba31b9c78ccc433a9cb78f43ca
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-02-04 16:05:59 +0100

    feat: WIP clamp movable circle

--- 2D/thumb_circle_touch.gd
+++ 2D/thumb_circle_touch.gd
@@ -5,22 +5,33 @@ extends Control
 @onready var circle: Control = $Circle
 @onready var follow_thumb: Polygon2D = $Circle/FollowThumb
 
+var max_radius = 150
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch:
-		print("touch pressed / released")
-		print(event.position)
+	#if event is InputEventScreenTouch:
+		#print("touch pressed / released")
+		#print(event.position)
 	if event is InputEventScreenDrag:
-		print("now dragging - position:")
-		print(event.position)
+		#print("now dragging - position:")
+		#print(event.position)
 		debug_touch_point.text = str(event.position)
 		
-		# TODO
 		# create thumb-indicator (circle shape)
 		# input-parameter for x/y position
 		var center_position: Vector2 = circle.position
 		# get position of center-point from thumb-indicator
 		var thumb_position: Vector2 = event.position
-		# set thumb-indicator to position
-		follow_thumb.position = thumb_position - center_position
+
 		# clamp position to pythagoras from center-point
 		# ignore input if too far from center-point
+		var direction = center_position - thumb_position
+		var distance = direction.length()
+		if distance > max_radius:
+			direction = direction.normalized() * max_radius
+			follow_thumb.position = thumb_position - center_position + direction
+			print(thumb_position - center_position + direction)
+		else:
+			# set thumb-indicator to position
+			follow_thumb.position = thumb_position - center_position
+			print(thumb_position - center_position)
+		# TODO: fix issues with global vs local coordinates
