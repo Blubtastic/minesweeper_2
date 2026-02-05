a6a781d97ca2e1ef6f0ac7c640bfb27c21a62094
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-05 21:59:17 +0100

    feat: joystick animation

--- 2D/joystick.gd
+++ 2D/joystick.gd
@@ -6,9 +6,16 @@ extends Control
 var touch_radius: int = max_radius * 2
 const INITIAL: Vector2 = Vector2(0,0)
 signal joystick_moved()
+var thumb_position = global_position
+var lerped_thump_position = thumb_position
 
 func _ready():
 	disable_if_not_touch()
+
+func _process(delta: float):
+	lerped_thump_position = lerp(lerped_thump_position, thumb_position, delta*100)
+	icon_movable.position = lerped_thump_position
+	queue_redraw()
 
 # Scoops up inputs not consumed by other things. Note that Control nodes covering an area will consume input before it reaches here.
 func _unhandled_input(event: InputEvent) -> void:
@@ -17,17 +24,13 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
 		if not event.is_pressed():
 			move_joystick()
-			# TODO: Maybe this code is a problem. If an event is NOT drag, reset joystick. 
-			# Cleanest would be to reset joystick of ALL events are not Drag. 
-
-
 
 func move_joystick(event: InputEventScreenDrag = null):
 	if event == null:
 		icon_movable.position = INITIAL
 		joystick_moved.emit(INITIAL, 1)
+		thumb_position = INITIAL
 		return
-	
 	var center: Vector2 = global_position
 	var thumb: Vector2 = event.position
 	var direction = center - thumb
@@ -36,9 +39,9 @@ func move_joystick(event: InputEventScreenDrag = null):
 	if distance > max_radius:
 		direction = direction.normalized() * max_radius
 		clamped_position = -direction
-	icon_movable.position = clamped_position
 	var speed = clamp(distance / max_radius, 0, 1)
 	joystick_moved.emit(clamped_position, speed)
+	thumb_position = clamped_position
 
 func disable_if_not_touch():
 	if DisplayServer.is_touchscreen_available():
@@ -47,3 +50,6 @@ func disable_if_not_touch():
 	else:
 		visible = false
 		set_process_mode(Node.PROCESS_MODE_DISABLED)
+
+func _draw():
+	draw_line(INITIAL, lerped_thump_position, Color.BLACK, 2.0)
