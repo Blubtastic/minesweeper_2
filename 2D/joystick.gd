5cbcee7a3dd449bda84b4d1ff75fb7c22b172ab5
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-05 22:09:42 +0100

    refactor: cleanup joystick script

--- 2D/joystick.gd
+++ 2D/joystick.gd
@@ -1,10 +1,8 @@
 extends Control
 
 @onready var icon_movable: Polygon2D = $ThumbIcon
-
 @export var max_radius: int = 100
-var touch_radius: int = max_radius * 2
-const INITIAL: Vector2 = Vector2(0,0)
+const ORIGIN: Vector2 = Vector2(0,0)
 signal joystick_moved()
 var thumb_position = global_position
 var lerped_thump_position = thumb_position
@@ -17,19 +15,22 @@ func _process(delta: float):
 	icon_movable.position = lerped_thump_position
 	queue_redraw()
 
+func _draw():
+	draw_line(ORIGIN, lerped_thump_position, Color.BLACK, 2.0)
+
 # Scoops up inputs not consumed by other things. Note that Control nodes covering an area will consume input before it reaches here.
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventScreenDrag:
 		move_joystick(event)
+	# Could be safer by doing this check for all event.index
 	if event is InputEventScreenTouch:
 		if not event.is_pressed():
 			move_joystick()
 
 func move_joystick(event: InputEventScreenDrag = null):
 	if event == null:
-		icon_movable.position = INITIAL
-		joystick_moved.emit(INITIAL, 1)
-		thumb_position = INITIAL
+		joystick_moved.emit(ORIGIN, 1)
+		thumb_position = ORIGIN
 		return
 	var center: Vector2 = global_position
 	var thumb: Vector2 = event.position
@@ -50,6 +51,3 @@ func disable_if_not_touch():
 	else:
 		visible = false
 		set_process_mode(Node.PROCESS_MODE_DISABLED)
-
-func _draw():
-	draw_line(INITIAL, lerped_thump_position, Color.BLACK, 2.0)
