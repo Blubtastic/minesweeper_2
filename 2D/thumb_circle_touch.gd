7574ec2a73cbc927b8c93d7cb0e31af8adebf486
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-02-05 08:12:16 +0100

    feat: clamp thumb icon to radius

--- 2D/thumb_circle_touch.gd
+++ 2D/thumb_circle_touch.gd
@@ -3,35 +3,26 @@ extends Control
 @onready var touch_screen_button: TouchScreenButton = $TouchScreenButton
 @onready var debug_touch_point: Label = $DebugTouchPoint
 @onready var circle: Control = $Circle
-@onready var follow_thumb: Polygon2D = $Circle/FollowThumb
+@onready var thumb_icon: Polygon2D = $Circle/ThumbIcon
 
-var max_radius = 150
+@export var max_radius = 150
 
 func _input(event: InputEvent) -> void:
-	#if event is InputEventScreenTouch:
-		#print("touch pressed / released")
-		#print(event.position)
 	if event is InputEventScreenDrag:
-		#print("now dragging - position:")
-		#print(event.position)
 		debug_touch_point.text = str(event.position)
-		
-		# create thumb-indicator (circle shape)
-		# input-parameter for x/y position
-		var center_position: Vector2 = circle.position
-		# get position of center-point from thumb-indicator
-		var thumb_position: Vector2 = event.position
+		move_thumb_icon(event)
 
-		# clamp position to pythagoras from center-point
-		# ignore input if too far from center-point
-		var direction = center_position - thumb_position
-		var distance = direction.length()
-		if distance > max_radius:
-			direction = direction.normalized() * max_radius
-			follow_thumb.position = thumb_position - center_position + direction
-			print(thumb_position - center_position + direction)
-		else:
-			# set thumb-indicator to position
-			follow_thumb.position = thumb_position - center_position
-			print(thumb_position - center_position)
-		# TODO: fix issues with global vs local coordinates
+func move_thumb_icon(event: InputEventScreenDrag):
+	var center: Vector2 = circle.global_position
+	var thumb: Vector2 = event.position
+
+	var direction = center - thumb
+	var distance = direction.length()
+	if distance > max_radius:
+		direction = direction.normalized() * max_radius
+		var final_position = -direction
+		thumb_icon.position = final_position
+		print(final_position, direction)
+	else:
+		var final_position = thumb - center
+		thumb_icon.position = final_position
