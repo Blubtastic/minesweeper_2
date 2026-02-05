62e1c90b1c500338b27d164834a98bd39bd15727
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-02-05 13:06:44 +0100

    feat: rename and restructure joystick

--- 2D/joystick.gd
+++ 2D/joystick.gd
@@ -3,9 +3,9 @@ extends Control
 @onready var touch_screen_button: TouchScreenButton = $TouchScreenButton
 @onready var debug_touch_point: Label = $DebugTouchPoint
 @onready var circle: Control = $Circle
-@onready var thumb_icon: Polygon2D = $Circle/ThumbIcon
+@onready var icon_movable: Polygon2D = $Circle/ThumbIcon
 
-@export var max_radius = 150
+@export var max_radius: int = 150
 const INITIAL: Vector2 = Vector2(0,0)
 signal joystick_moved()
 
@@ -15,25 +15,27 @@ func _ready():
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenDrag:
 		debug_touch_point.text = str(event.position)
-		move_thumb_icon(event)
+		move_joystick(event)
 	if event is InputEventScreenTouch:
 		if not event.is_pressed():
-			print("released")
-			thumb_icon.position = INITIAL
-			joystick_moved.emit(INITIAL)
+			move_joystick()
 
-func move_thumb_icon(event: InputEventScreenDrag):
+func move_joystick(event: InputEventScreenDrag = null):
+	if event == null:
+		icon_movable.position = INITIAL
+		joystick_moved.emit(INITIAL)
+		return
+	
 	var center: Vector2 = circle.global_position
 	var thumb: Vector2 = event.position
-
 	var direction = center - thumb
 	var distance = direction.length()
-	var final_position: Vector2 = thumb - center
+	var clamped_position: Vector2 = thumb - center
 	if distance > max_radius:
 		direction = direction.normalized() * max_radius
-		final_position = -direction
-	thumb_icon.position = final_position
-	joystick_moved.emit(final_position)
+		clamped_position = -direction
+	icon_movable.position = clamped_position
+	joystick_moved.emit(clamped_position)
 
 func disable_if_not_touch():
 	if DisplayServer.is_touchscreen_available():
