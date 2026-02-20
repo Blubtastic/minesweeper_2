dc012b870e5d0d09f45fb84e81d3fe0565ac9c8e
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-20 19:47:28 +0100

    refactor: move game_over into folder

--- 2D/menu_button.gd
+++ 2D/menu_button.gd
@@ -1,5 +1,5 @@
 extends Button
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://2D/main_menu.tscn")
+	get_tree().change_scene_to_file("res://2D/menu.tscn")
 	Globals.reset_game()
