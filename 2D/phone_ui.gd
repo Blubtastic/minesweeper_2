186e83d0de20640a327522d804252edfd8cd3ea8
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-20 19:45:45 +0100

    refactor: delete old phone ui

--- 2D/phone_ui.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-@onready var phone_only: Control = $PhoneOnly
-
-func _ready() -> void:
-	if DisplayServer.is_touchscreen_available():
-		phone_only.visible = true
-	else:
-		phone_only.visible = false
