ff1aa4aa216efa4a04528fd20d6560d3045fc264
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-21 10:18:21 +0100

    refactor: move stuff out of features

--- 2D/go_to_menu_button.gd
+++ 2D/go_to_menu_button.gd
@@ -1,5 +1,5 @@
 extends Button
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://features/menu/menu.tscn")
+	get_tree().change_scene_to_file("res://menu/menu.tscn")
 	Globals.reset_game()
