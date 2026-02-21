e7a15f7e0f630be4ca45af003905c309556cc68e
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-21 12:32:46 +0100

    refactor: error message load next_scene

--- app/loading.gd
+++ app/loading.gd
@@ -2,11 +2,13 @@ extends Node3D
 @onready var poof: GPUParticles3D = $Particles/Poof
 @onready var sparks: GPUParticles3D = $Particles/Sparks
 @onready var tire_debris_snow: Node3D = $Particles/TireDebrisSnow
-@export var MAIN_MENU: Resource
+@export var NEXT_SCENE: Resource
 
 func _ready():
 	poof.restart()
 	sparks.restart()
 	await get_tree().create_timer(0.5).timeout
-	if MAIN_MENU:
-		get_tree().change_scene_to_packed(MAIN_MENU)
+	if NEXT_SCENE:
+		get_tree().change_scene_to_packed(NEXT_SCENE)
+	else:
+		print("Can't change scene to NEXT_SCENE, value is ", NEXT_SCENE, ".")
