3bfd734f63dc727ed3c7aae9015d6beec6977b46
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-21 11:21:34 +0100

    refactor: replace button scripts with signals

--- 2D/go_to_main_button.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-const MAIN = preload("uid://c1p7v7melj8n4")
-
-func _on_pressed() -> void:
-	get_tree().change_scene_to_packed(MAIN)
--- menu/menu_2d.gd
+++ menu/menu_2d.gd
@@ -1,8 +1,9 @@
 extends Control
 
 const MAIN = preload("uid://c1p7v7melj8n4")
+
 @export var version_number := '0.0.0'
-@onready var version_num: Label = $Version/VersionNum
+@onready var version_num: Label = $AnchorBottomRight/VersionNum
 
 func _ready() -> void:
 	version_num.text = 'v' + version_number
