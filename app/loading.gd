c5f4707ffb1dbce5df0c9fa7d34be89b1c966d90
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-02-21 12:30:16 +0100

    refactor: remove scene dependency from loading

--- app/loading.gd
+++ app/loading.gd
@@ -2,10 +2,11 @@ extends Node3D
 @onready var poof: GPUParticles3D = $Particles/Poof
 @onready var sparks: GPUParticles3D = $Particles/Sparks
 @onready var tire_debris_snow: Node3D = $Particles/TireDebrisSnow
-const MAIN_MENU = preload("uid://celxavas2i8qt")
+@export var MAIN_MENU: Resource
 
 func _ready():
 	poof.restart()
 	sparks.restart()
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_packed(MAIN_MENU)
+	if MAIN_MENU:
+		get_tree().change_scene_to_packed(MAIN_MENU)
