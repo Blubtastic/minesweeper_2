ee2a3e55f2a96529e0acc1d99377bac6f6f39d74
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-02-27 09:49:47 +0100

    feat: tween world_speed to 0 on game_over

--- context/globals.gd
+++ context/globals.gd
@@ -2,16 +2,23 @@ extends Node
 
 var player_hp: int
 var score = 0
-var world_speed = 1
+var initial_world_speed: float = 1.0
+var world_speed: float = 1.0
 var game_over: bool = false
 
 signal game_ended()
+
+func set_world_speed(speed: float):
+	world_speed = speed
 
 func end_game():
 	if game_over == false:
 		game_ended.emit()
 		game_over = true
+		var tween = get_tree().create_tween()
+		tween.tween_method(set_world_speed, world_speed, 0, 2)
 
 func reset_game():
 	score = 0
 	game_over = false
+	world_speed = initial_world_speed
