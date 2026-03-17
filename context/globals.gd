de70dbcbdc9d325d040c0577630139e11baa60d8
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-03-17 09:01:11 +0100

    feat: move world_speed by player

--- context/globals.gd
+++ context/globals.gd
@@ -1,19 +1,25 @@
 extends Node
 
-var player_hp: int
-var score = 0
-var score_2 = 0
 var initial_world_speed: float = 1.0
 var world_speed: float = 1.0
 var game_over: bool = false
+var game_mode: float = 0 # 0 is Stress, 1 is Chill
+
+var score = 0
+var player_hp: int
+var player1_position: Vector3 = Vector3.ZERO
 
 # global parameter is_2p
+var score_2 = 0
 
 signal game_ended()
 signal start_exploded_cube_effects()
 
 func set_world_speed(speed: float):
 	world_speed = speed
+
+func set_player1_position(position: Vector3):
+	player1_position = position
 
 func player_died():
 	if game_over == false:
@@ -28,3 +34,10 @@ func reset_game():
 
 func exploded_cube_effects():
 	start_exploded_cube_effects.emit()
+
+func _physics_process(_delta: float):
+	if game_mode == 1:
+		if player1_position.z > 1:
+			Globals.set_world_speed(0)
+		else:
+			Globals.set_world_speed(1)
--- menu_main/menu_main_ui.gd
+++ menu_main/menu_main_ui.gd
@@ -9,4 +9,9 @@ func _ready() -> void:
 	version_num.text = 'v' + version_number
 
 func _on_play_pressed() -> void:
+	Globals.game_mode = 0 # Chill mode
+	get_tree().change_scene_to_packed(MAIN)
+
+func _on_endless_mode_pressed() -> void:
+	Globals.game_mode = 1 # Stress mode
 	get_tree().change_scene_to_packed(MAIN)
