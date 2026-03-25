0375c967e1904d1e17fd389cf101600a7587e9f6
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-03-25 15:59:19 +0100

    feat: polish on camera jump feature

--- context/camera/camera_jump.gd
+++ context/camera/camera_jump.gd
@@ -2,7 +2,6 @@ extends Node3D
 
 const CAMERA_JUMP_CURVE = preload("uid://ckyoktsd8fghh")
 var initial_y: float = 0
-var intensity = 0.5
 
 func _ready():
 	initial_y = position.y
@@ -10,7 +9,10 @@ func _ready():
 
 func on_cube_exploded():
 	var tween = get_tree().create_tween()
-	tween.tween_method(camera_jump, 0.0, 1.0, 1.2)
+	if Globals.player_hp < 2: # unclean - depends on player_hp. Instead, improve signal to fire with updated game_over info.
+		tween.tween_method(camera_jump.bind(10), 0.0, 0.4, .2)
+	else:
+		tween.tween_method(camera_jump, 0.0, 1.0, 1.2)
 
-func camera_jump(elapsed_time: float):
+func camera_jump(elapsed_time: float, intensity: float = 0.5):
 	position.y = initial_y + (CAMERA_JUMP_CURVE.sample(elapsed_time)*intensity)
