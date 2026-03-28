316b3e8bacfc061d6e24defa78ecdedf416eb4a1
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-03-28 15:47:17 +0100

    feat: spawn two players with unique inputs

--- main/main.gd
+++ main/main.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
 const GAME_OVER = preload("uid://ck8cc332mqpua")
-const GAME_PLAYER = preload("uid://b0boueut21rwe")
-@onready var game_player: Node3D = $GamePlayer
+const _1_PLAYER = preload("uid://do0hnve2ge0ub")
+const _2_PLAYERS = preload("uid://ccotdq6huom6h")
 
 @onready var hud: Control = $HUD
+
 
 func _ready():
 	Globals.reset_game()
@@ -13,15 +14,22 @@ func _ready():
 	Music.stop_cozy()
 	Music.mute_drums(true)
 	Music.mute_tambourine(true)
+	
 	if Globals.is_2p:
-		var player_2 = GAME_PLAYER.instantiate()
-		player_2.position = Vector3(game_player.position.x+3, game_player.position.y, game_player.position.z)
+		var player_2 = _2_PLAYERS.instantiate()
+		player_2.position = Vector3(0,1,0)
 		add_child(player_2)
+	else:
+		var player_1 = _1_PLAYER.instantiate()
+		player_1.position = Vector3(0,1,0)
+		add_child(player_1)
+
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.is_action_pressed("restart"):
 			get_tree().reload_current_scene()
+
 
 func _on_game_ended():
 	hud.visible = false
@@ -30,7 +38,3 @@ func _on_game_ended():
 	Music.kill_music()
 	var tween = create_tween()
 	tween.tween_method(Globals.set_world_speed, Globals.world_speed, 0, 2)
-
-# TODO 2: control world_speed increase/decrease from here
-	# One mode for gradually increase it (Stress mode)
-	# One mode for setting it to player_speed if player.position.z < -3 or something (Chill mode)
