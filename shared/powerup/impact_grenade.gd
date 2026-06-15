71d9d0ad048832105b171189202fef33eabcb0f2
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-06-15 09:32:03 +0200

    feat: p1 of consistent cube points

--- shared/powerup/impact_grenade.gd
+++ shared/powerup/impact_grenade.gd
@@ -3,8 +3,11 @@ extends RigidBody3D
 @export var source: Node
 @onready var clear_hitbox: Area3D = $ClearHitbox
 var detected_cubes := []
+var fired_by: Node3D # set to player in inspector, replaces source.
 
-func _on_area_3d_area_entered(_area: Area3D) -> void:
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.has_method("damage"):
+		area.damage(source) # TODO: pass self as arg to give points
 	detected_cubes = clear_hitbox.get_overlapping_areas().filter(
 		func (cube: Area3D) -> bool: return cube.has_method("damage")
 	)
