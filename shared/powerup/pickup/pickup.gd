e1278e12e371895e4ff098f01bacc67eb9b92156
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-06-22 10:06:52 +0200

    feat: powerup impact grenade p1

--- shared/powerup/pickup/pickup.gd
+++ shared/powerup/pickup/pickup.gd
@@ -4,6 +4,17 @@ class_name Pickup
 
 @export var rotation_speed: float = 10
 
+const IMPACT_GRENADE = preload("uid://5s7j7ty55jtj")
+var powerups: Dictionary[String, PackedScene] = {
+	'impact_grenade': IMPACT_GRENADE
+}
+var powerup: PackedScene = powerups['impact_grenade']
+
+
 func _physics_process(delta: float) -> void:
 	# Rotate around the Y axis
 	rotate_y(rotation_speed * delta)
+
+
+func pick_up() -> void:
+	queue_free()
