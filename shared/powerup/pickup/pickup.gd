7f897350f5608864da3f456c7a5b6d78b9586c00
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-06-22 10:23:00 +0200

    feat: p2 powerup scene and code

--- shared/powerup/pickup/pickup.gd
+++ shared/powerup/pickup/pickup.gd
@@ -2,14 +2,20 @@ extends Node3D
 
 class_name Pickup
 
-@export var rotation_speed: float = 10
+
+@export var rotation_speed: float = 2
 
 const IMPACT_GRENADE = preload("uid://5s7j7ty55jtj")
+@onready var bomb: Node3D = $bomb
 var powerups: Dictionary[String, PackedScene] = {
 	'impact_grenade': IMPACT_GRENADE
 }
 var powerup: PackedScene = powerups['impact_grenade']
 
+
+## TODO: when adding a new powerup, ask AI how to add an @export for choosing from a dict.
+func _ready() -> void:
+	bomb.visible = true
 
 func _physics_process(delta: float) -> void:
 	# Rotate around the Y axis
