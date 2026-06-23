ccead6a7be794d4b5f4d3145a0dfdfde049cf7ca
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-06-23 17:47:31 +0200

    refactor: move visualEffects to its own script and node

--- game_player/player/player.gd
+++ game_player/player/player.gd
@@ -1,51 +1,17 @@
 extends CharacterBody3D
 
-
 class_name Player
 
-
-# ==================== MOVEMENT PARAMETERS ====================
-#@export var base_speed: float = 5.0  # Base movement speed
-#@export var forward_speed_bonus: float = 2.0  # Extra speed when moving forward
-#@export var speed_multiplier: float = 1.0  # Current speed intensity (0-1 range)
-#var external_speed: float = 0.0  # Speed applied externally (e.g., by moving platforms)
-#const ACCELERATION = 80
-#const JUMP_VELOCITY = 6.0
-#const ROTATION_DAMPING = 30.0  # Higher = less rotation tilt
-#const DAMAGE_VELOCITY = 12.0
-#const DEATH_TERMINAL_VELOCITY = 40.0
-
-
-# ==================== GAMEPLAY CONFIG ====================
 @export_range(1,2) var player_id := 1
 @export var hp: int = 3
-var is_dead: bool = false  # Track if player is dead
+var is_dead: bool = false
 var joystick_direction: Vector2 = Vector2.ZERO  # Joystick input accumulator
 
-
-# ==================== VISUALS ====================
-const JUMP_PARTICLE_OFFSET_Y = -0.16
-const DEBRIS_PARTICLE_OFFSET_Y = -0.16
-const SHIELD_OPACITY_GOAL_ACTIVE = 0.5
-const SHIELD_OPACITY_GOAL_INACTIVE = 0.0
-const SHIELD_OPACITY_LERP_SPEED = 15.0
-const INVINCIBILITY_DURATION = 1.0
-const INVINCIBILITY_RECOVERY_DELAY = 0.5
-const TRAIL_VFX_DURATION = 1.0
-const TRAIL_VFX_FADE_DURATION = 1.0
-var shield_opacity: float = 0.0  # Current shield visual opacity
-const TRAIL_VFX = preload("uid://drynt1383xlht")
-@onready var shield_mesh: MeshInstance3D = $ShieldMesh
-@onready var left_debris: Node3D = $TireDebrisSnowLeft
-@onready var right_debris: Node3D = $TireDebrisSnowRight
-@export var sparks: PackedScene
-@export var poof: PackedScene
-
-@onready var cube_hitbox: Area3D = $CubeHitbox
-
-
-# ==================== IMPORT SCRIPTS & SIGNALS ====================
 var player_movement := PlayerMovement.new(self)
+@onready var visual_effects := $VisualEffects
+
+## Will be removed
+@onready var cube_hitbox: Area3D = $CubeHitbox
 
 signal is_flying_changed(is_flying: bool)
 signal was_damaged(current_hp: int)
@@ -54,7 +20,7 @@ signal was_damaged(current_hp: int)
 func _physics_process(delta: float) -> void:
 	player_movement.handle_base_movement(delta)
 
-	## MOVE HORIZONTALLY
+	## Input setup could be moved to its own class.
 	var input_dir := Input.get_vector(
 		"move_left_player" + str(player_id),
 		"move_right_player" + str(player_id),
@@ -67,64 +33,10 @@ func _physics_process(delta: float) -> void:
 	## JUMP
 	if is_on_floor() and Input.is_action_just_pressed("jump_player" + str(player_id)):
 		player_movement.jump()
-		## TODO: poof-particle
-		# e.g: fire_oneshot_particle(poof, JUMP_PARTICLE_OFFSET_Y)
+		visual_effects.fire_poof_below_player()
 
-	## Tire debris
-	update_horizontal_debris(direction)
-
-	## TODO: disconnect visuals from movement
-	update_visuals(delta)
-
-	#update_jump_and_gravity(delta)
-	#update_rotation_tilt()
-	#move_and_slide()
-
-
-
-# ==================== JUMP AND GRAVITY ====================
-#func update_jump_and_gravity(delta: float) -> void:
-	#if is_on_floor():
-		#if Input.is_action_just_pressed("jump_player" + str(player_id)):
-			#jump()
-	#else:
-		#apply_gravity(delta)
-
-
-#func jump() -> void:
-	#velocity.y = JUMP_VELOCITY
-	#fire_oneshot_particle(poof, JUMP_PARTICLE_OFFSET_Y)
-#
-#func apply_gravity(delta: float) -> void:
-	#velocity += get_gravity() * 2.0 * delta
-
-
-
-#func apply_movement(direction: Vector3, delta: float) -> void:
-	## Lateral movement
-	#var max_x_velocity := direction.x * base_speed * speed_multiplier
-	#velocity.x = move_toward(velocity.x, max_x_velocity, ACCELERATION * delta)
-#
-	## Forward/backward movement (with directional bonus)
-	#var z_speed := base_speed if direction.z > 0 else base_speed + forward_speed_bonus
-	#var max_z_velocity := direction.z * z_speed * speed_multiplier + external_speed
-	#velocity.z = move_toward(velocity.z, max_z_velocity, ACCELERATION * delta)
-
-
-#func apply_deceleration(delta: float) -> void:
-	#var decel_rate := ACCELERATION * delta
-	#velocity.x = move_toward(velocity.x, 0.0, decel_rate)
-	#velocity.z = move_toward(velocity.z, external_speed, decel_rate)
-
-
-#func launch_self_upwards() -> void:
-	#velocity.y = DEATH_TERMINAL_VELOCITY if is_dead else DAMAGE_VELOCITY
-
-
-# ==================== ROTATION AND TILT ====================
-#func update_rotation_tilt() -> void:
-	#rotation.z = -velocity.x / ROTATION_DAMPING
-	#rotation.x = -velocity.z / ROTATION_DAMPING
+	visual_effects.update_horizontal_debris(direction, hp)
+	visual_effects.update_visuals(delta)
 
 
 # ==================== DAMAGE AND KNOCKBACK ====================
@@ -140,91 +52,14 @@ func damage() -> void:
 
 	# Trigger invincibility period
 	Globals.players_invincible = true
-	spawn_damage_trail_vfx()
-	await get_tree().create_timer(INVINCIBILITY_DURATION).timeout
+	visual_effects.spawn_damage_trail_vfx()
+	await get_tree().create_timer(1.0).timeout
 
 	Globals.players_invincible = false
 	is_flying_changed.emit(false)
 
-	await get_tree().create_timer(INVINCIBILITY_RECOVERY_DELAY).timeout
-	cleanup_damage_trail_vfx()
-
-
-func spawn_damage_trail_vfx() -> void:
-	var trail_vfx := TRAIL_VFX.instantiate()
-	add_child(trail_vfx)
-	trail_vfx.name = "DamageTrailVFX"
-
-
-func cleanup_damage_trail_vfx() -> void:
-	await get_tree().create_timer(TRAIL_VFX_DURATION).timeout
-	var trail_vfx := get_node_or_null("DamageTrailVFX")
-	if not trail_vfx:
-		return
-
-	trail_vfx.get_node("Smoke").emitting = false
-	trail_vfx.get_node("Fire").emitting = false
-
-	await get_tree().create_timer(TRAIL_VFX_FADE_DURATION).timeout
-	trail_vfx.queue_free()
-
-
-# ==================== VISUAL UPDATES ====================
-func update_horizontal_debris(direction: Vector3) -> void:
-	if direction and hp > 0:
-		update_debris(is_on_floor())
-	else:
-		update_debris(false)
-
-
-func update_visuals(delta: float) -> void:
-	update_collision_particles()
-	update_shield_visual(delta)
-
-
-func update_shield_visual(delta: float) -> void:
-	var mat := shield_mesh.get_active_material(0)
-	if not mat or not mat is StandardMaterial3D:
-		return
-
-	var goal_opacity := SHIELD_OPACITY_GOAL_ACTIVE if Globals.players_invincible else SHIELD_OPACITY_GOAL_INACTIVE
-	shield_opacity = lerpf(shield_opacity, goal_opacity, delta * SHIELD_OPACITY_LERP_SPEED)
-	mat.albedo_color.a = shield_opacity
-
-
-func update_collision_particles() -> void:
-	var collision := get_last_slide_collision()
-	if collision and collision.get_angle() == 0.0:
-		fire_oneshot_particle(sparks, DEBRIS_PARTICLE_OFFSET_Y)
-
-
-# ==================== PARTICLE EFFECTS ====================
-func fire_particle(particle: PackedScene) -> void:
-	var particle_instance := particle.instantiate()
-	particle_instance.transform.origin = transform.origin
-	add_sibling(particle_instance)
-
-
-func fire_oneshot_particle(scene: PackedScene, offset_y: float) -> void:
-	var instance := scene.instantiate()
-	add_sibling(instance)
-	instance.global_position = Vector3(global_position.x, global_position.y + offset_y, global_position.z)
-
-
-func emit_debris() -> void:
-	left_debris.emit_debris()
-	right_debris.emit_debris()
-
-func stop_debris() -> void:
-	left_debris.stop_debris()
-	right_debris.stop_debris()
-
-func update_debris(should_emit: bool) -> void:
-	if should_emit:
-		emit_debris()
-	else:
-		stop_debris()
-
+	await get_tree().create_timer(0.5).timeout
+	visual_effects.cleanup_damage_trail_vfx()
 
 
 # ==================== COLLISION CALLBACKS ====================
---
+++ game_player/player/visual_effects.gd
@@ -0,0 +1,85 @@
+extends Node3D
+
+
+# ==================== VISUALS ====================
+const DEBRIS_PARTICLE_OFFSET_Y = -0.16
+const SHIELD_OPACITY_GOAL_ACTIVE = 0.5
+const SHIELD_OPACITY_GOAL_INACTIVE = 0.0
+const SHIELD_OPACITY_LERP_SPEED = 15.0
+const TRAIL_VFX_DURATION = 1.0
+const TRAIL_VFX_FADE_DURATION = 1.0
+var shield_opacity: float = 0.0  # Current shield visual opacity
+
+const POOF = preload("uid://ddwftyj3tif34")
+const SPARKS = preload("uid://dvabslbqfwp0v")
+const TRAIL_VFX = preload("uid://drynt1383xlht")
+@onready var left_debris: Node3D = $LeftDebris
+@onready var right_debris: Node3D = $RightDebris
+@onready var shield_mesh: MeshInstance3D = $ShieldMesh
+
+@export var p: Player
+
+
+func spawn_damage_trail_vfx() -> void:
+	var trail_vfx := TRAIL_VFX.instantiate()
+	add_child(trail_vfx)
+	trail_vfx.name = "DamageTrailVFX"
+
+
+func cleanup_damage_trail_vfx() -> void:
+	await get_tree().create_timer(TRAIL_VFX_DURATION).timeout
+	var trail_vfx := get_node_or_null("DamageTrailVFX")
+	if not trail_vfx:
+		return
+
+	trail_vfx.get_node("Smoke").emitting = false
+	trail_vfx.get_node("Fire").emitting = false
+
+	await get_tree().create_timer(TRAIL_VFX_FADE_DURATION).timeout
+	trail_vfx.queue_free()
+
+
+func update_visuals(delta: float) -> void:
+	update_collision_particles()
+	update_shield_visual(delta)
+
+
+func update_shield_visual(delta: float) -> void:
+	var mat := shield_mesh.get_active_material(0)
+	if not mat or not mat is StandardMaterial3D:
+		return
+
+	var goal_opacity := SHIELD_OPACITY_GOAL_ACTIVE if Globals.players_invincible else SHIELD_OPACITY_GOAL_INACTIVE
+	shield_opacity = lerpf(shield_opacity, goal_opacity, delta * SHIELD_OPACITY_LERP_SPEED)
+	mat.albedo_color.a = shield_opacity
+
+
+func update_collision_particles() -> void:
+	var collision := p.get_last_slide_collision()
+	if collision and collision.get_angle() == 0.0:
+		var instance := SPARKS.instantiate()
+		add_sibling(instance)
+		instance.emitting = true
+		instance.global_position = Vector3(p.global_position.x, p.global_position.y -0.16, p.global_position.z)
+
+
+func fire_poof_below_player() -> void:
+	var poof_instance := POOF.instantiate()
+	add_sibling(poof_instance)
+	poof_instance.emitting = true
+	poof_instance.global_position = Vector3(p.global_position.x, p.global_position.y -0.16, p.global_position.z)
+
+
+func update_horizontal_debris(direction: Vector3, hp: int) -> void:
+	if direction and hp > 0:
+		update_debris(p.is_on_floor())
+	else:
+		update_debris(false)
+
+func update_debris(should_emit: bool) -> void:
+	if should_emit:
+		left_debris.emit_debris()
+		right_debris.emit_debris()
+	else:
+		left_debris.stop_debris()
+		right_debris.stop_debris()
