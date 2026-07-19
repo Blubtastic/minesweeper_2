24cf2237054134691c1c5ea3bef0526b59589055
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-07-19 22:51:03 +0200

    feat: animation on button

--- shared/ui/button.gd
+++ shared/ui/button.gd
@@ -1,7 +1,20 @@
 extends Button
 
 @export var button_text: String
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	if button_text:
 		text = button_text
+
+
+func _on_focus_entered() -> void:
+	animate_focus()
+
+
+func _on_mouse_entered() -> void:
+	animate_focus()
+
+
+func animate_focus() -> void:
+	animation_player.play("pop")
