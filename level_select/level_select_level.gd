c608998c48d1190f73c1c39dc176a769be40a8ae
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-07-19 21:59:14 +0200

    feat: logic for starting level

--- level_select/level_select_level.gd
+++ level_select/level_select_level.gd
@@ -5,5 +5,8 @@ extends Area3D
 signal was_hovered(position: Vector3, level: int)
 
 
-func _on_input_event(_camera: Node, _event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	was_hovered.emit(position, level)
+	
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("START LEVEL ", level)
