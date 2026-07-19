91fb3a1942dcbeccd2dbcd1f203f47b162492237
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2026-07-19 21:57:52 +0200

    feat: car moves to selected level

--- level_select/level_select_level.gd
+++ level_select/level_select_level.gd
@@ -2,8 +2,8 @@ extends Area3D
 
 @export var level: int = 1
 
-signal was_clicked()
+signal was_hovered(position: Vector3, level: int)
+
 
 func _on_input_event(_camera: Node, _event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	was_clicked.emit(position, level)
-	print(level, "clicked")
+	was_hovered.emit(position, level)
--- level_select/world_map.gd
+++ level_select/world_map.gd
@@ -1,14 +1,13 @@
 extends Node3D
 
 @onready var player_model: Node3D = $PlayerModel
-var levels := [$Levels/LevelSelectLevel1, $Levels/LevelSelectLevel2, $Levels/LevelSelectLevel3, $Levels/LevelSelectLevel4]
+@onready var levels: Array[Area3D] = [$Levels/LevelSelectLevel1, $Levels/LevelSelectLevel2, $Levels/LevelSelectLevel3, $Levels/LevelSelectLevel4]
 
 
 func _ready() -> void:
 	for level in levels:
-		print(level)
-		#level.was_clicked.connect(move_to_position)
+		level.was_hovered.connect(move_to_position)
 
 
-func move_to_position(new_position: Vector3) -> void:
-	player_model.position = new_position
+func move_to_position(new_position: Vector3, _level: int) -> void:
+	player_model.position = Vector3(new_position.x, player_model.position.y, new_position.z)
