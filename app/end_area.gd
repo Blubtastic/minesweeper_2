4edd8788504882f7c10a45553006416787d69a70
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-07-21 16:01:25 +0200

    feat: show end_area_ui on finish

--- app/end_area.gd
+++ app/end_area.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 #const MoveWithGlobal = preload("uid://cdvo4jdy5fco4")
 @onready var area_3d: Area3D = $Area3D
+@onready var end_area_ui: Control = $EndAreaUI
 
 
 ## Moves the particle to match the world speed.
@@ -10,4 +11,5 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
-		Globals.end_game()
+		#Globals.end_game()
+		end_area_ui.visible = true
