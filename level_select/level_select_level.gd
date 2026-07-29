68ac6405a412a0829b3ad2524dd0fde75f6d25b3
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-07-29 09:50:56 +0200

    refactor: clean up code

--- level_select/level_select_level.gd
+++ level_select/level_select_level.gd
@@ -24,15 +24,15 @@ func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3,
 		change_to_level()
 
 
-
 func change_to_level() -> void:
 	Globals.current_level = level
-	get_tree().change_scene_to_file('res://levels/level_' + str(int(level)) + '.tscn')
+	var path := 'res://levels/level_' + str(int(group)) + '_' + str(int(level)) + '.tscn'
+	get_tree().change_scene_to_file(path)
+
 
 # HANDLE VISUAL FOCUS
 func grab_focus() -> void:
 	$FocusMesh.visible = true
-
 
 func release_focus() -> void:
 	$FocusMesh.visible = false
