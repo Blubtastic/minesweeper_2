bf20056a56382a5a3173d573101689550b683541
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-07-29 09:28:40 +0200

    feat: keyboard support switching levels

--- level_select/level_select_level.gd
+++ level_select/level_select_level.gd
@@ -1,8 +1,14 @@
 extends Area3D
+class_name LevelSelectLevel
 
+@onready var level_name: Label3D = $LevelName
 @export var group: int = 1
 @export var level: int = 1
-@onready var level_name: Label3D = $LevelName
+
+@export var left: Area3D
+@export var right: Area3D
+@export var up: Area3D
+@export var down: Area3D
 
 signal was_hovered(position: Vector3, level: int)
 
@@ -10,10 +16,20 @@ signal was_hovered(position: Vector3, level: int)
 func _ready() -> void:
 	level_name.text = str(group) + "-" + str(level)
 
+
+# HANDLE LEFT CLICK
 func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	was_hovered.emit(position, level)
 
-	# LEVEL SELECT (on left click)
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		Globals.current_level = level
 		get_tree().change_scene_to_file('res://levels/level_' + str(int(level)) + '.tscn')
+
+
+# HANDLE VISUAL FOCUS
+func grab_focus() -> void:
+	$FocusMesh.visible = true
+
+
+func release_focus() -> void:
+	$FocusMesh.visible = false
--- level_select/world_map.gd
+++ level_select/world_map.gd
@@ -1,13 +1,36 @@
 extends Node3D
 
 @onready var player_model: Node3D = $PlayerModel
-@onready var levels: Array[Area3D] = [$Levels/SelectLevel1_1, $Levels/SelectLevel1_2, $Levels/SelectLevel1_3, $Levels/SelectLevel1_4, $Levels/SelectLevel2_1, $Levels/SelectLevel2_2, $Levels/SelectLevel2_3, $Levels/SelectLevel3_1, $Levels/SelectLevel3_2, $Levels/SelectLevel3_3, $Levels/SelectLevel3_4]
+@onready var levels: Array[LevelSelectLevel] = [$Levels/SelectLevel1_1, $Levels/SelectLevel1_2, $Levels/SelectLevel1_3, $Levels/SelectLevel1_4, $Levels/SelectLevel2_1, $Levels/SelectLevel2_2, $Levels/SelectLevel2_3, $Levels/SelectLevel3_1, $Levels/SelectLevel3_2, $Levels/SelectLevel3_3, $Levels/SelectLevel3_4]
+@onready var initially_selected_level: LevelSelectLevel = $Levels/SelectLevel1_1
 
+var selected_level: LevelSelectLevel
+#var level_selects: Array = []
 
 func _ready() -> void:
+	selected_level = initially_selected_level
 	for level in levels:
 		level.was_hovered.connect(move_to_position)
-
+	#level_selects = get_tree().get_nodes_in_group("level_select_buttons")
 
 func move_to_position(new_position: Vector3, _level: int) -> void:
 	player_model.position = Vector3(new_position.x, player_model.position.y, new_position.z)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.is_action_pressed("ui_left") and selected_level.left:
+			selected_level = selected_level.left
+		if event.is_action_pressed("ui_right") and selected_level.right:
+			selected_level = selected_level.right
+		if event.is_action_pressed("ui_up") and selected_level.up:
+			selected_level = selected_level.up
+		if event.is_action_pressed("ui_down") and selected_level.down:
+			selected_level = selected_level.down
+	handle_focus_change()
+
+
+func handle_focus_change() -> void:
+	for level in levels:
+		level.release_focus()
+	selected_level.grab_focus()
