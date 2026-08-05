d07b466069ddc29206561f36f7b7d568168f41a8
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2026-08-05 09:21:45 +0200

    feat: black background reward icons

--- app/end_area_ui.gd
+++ app/end_area_ui.gd
@@ -2,6 +2,10 @@ extends Control
 
 const WORLD_MAP = preload("uid://dec4vccolfnnv")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var icon_clear: PanelContainer = $VBoxContainer/CenterChildren/CenterPanel/VBoxContainer/IconMargin/Icons/IconClear
+@onready var icon_ace: PanelContainer = $VBoxContainer/CenterChildren/CenterPanel/VBoxContainer/IconMargin/Icons/IconAce
+@onready var icon_full_clear: PanelContainer = $VBoxContainer/CenterChildren/CenterPanel/VBoxContainer/IconMargin/Icons/IconFullClear
+
 
 
 func _on_restart_pressed() -> void:
@@ -18,5 +22,8 @@ func _on_next_level_pressed() -> void:
 
 
 func handle_game_over() -> void:
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.25).timeout
 	animation_player.play("show_rewards")
+	icon_clear.visible = true
+	icon_ace.visible = false
+	icon_full_clear.visible = true
